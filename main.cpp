3bd75455a57c0d280e4de926ee6c81a283821393
touchdesky27 <ewardsnguyen@gmail.com>
2020-11-01 23:48:13 +0700

    [UI] Update main icon: remove background and added folder for output button icon

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowIcon(QIcon("./resources/icons/main_ic.jpg"));
+    w.setWindowIcon(QIcon("./resources/icons/main_ic.png"));
     w.show();
     return a.exec();
 }
